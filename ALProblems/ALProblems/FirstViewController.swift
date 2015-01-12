8c39ade52c2ed2a626a6a7f169be911001ccf636
Jack Cox <jcox@captechconsulting.com>
2015-01-12 07:22:54 -0500

    Initial checkin

---
+++ ALProblems/ALProblems/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ALProblems
+//
+//  Created by Jack Cox on 1/12/15.
+//  Copyright (c) 2015 CapTech Consulting. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
